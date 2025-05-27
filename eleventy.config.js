4c70320bcf11b36c67d6d4ea460fcb7479d6d665
beyonddream <43626691+beyonddream@users.noreply.github.com>
2025-05-26 19:10:59 -0700

    Update css style + misc changes

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,6 @@
 export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("styles");
+
+    // Add shortcode for current year
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 };
