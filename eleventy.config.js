733a1af94f09a00b2f557cc37547c833e8309834
beyonddream <43626691+beyonddream@users.noreply.github.com>
2025-06-01 12:21:13 -0700

    cleanup css + whole lot of UX improvements

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,43 @@
-export default function(eleventyConfig) {
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginNavigation = require("@11ty/eleventy-navigation");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+const { DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+    
+    // Add plugins
+    eleventyConfig.addPlugin(pluginRss);
+    eleventyConfig.addPlugin(pluginSyntaxHighlight);
+    eleventyConfig.addPlugin(pluginNavigation);
+    
     eleventyConfig.addPassthroughCopy("styles");
 
-    // Add shortcode for current year
-    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+    // Customize Markdown library and settings:
+    let markdownLibrary = markdownIt({
+        html: true,
+        breaks: true,
+        linkify: true,
+    }).use(markdownItAnchor, {
+        permalink: markdownItAnchor.permalink.ariaHidden({
+        placement: "after",
+        class: "direct-link",
+        symbol: "#",
+        level: [1, 2, 3, 4],
+        }),
+        slugify: eleventyConfig.getFilter("slug"),
+    });
+    eleventyConfig.setLibrary("md", markdownLibrary);
+
+    eleventyConfig.addFilter("readableDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+          "dd LLL yyyy"
+        );
+    });
+
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+    });
 };
